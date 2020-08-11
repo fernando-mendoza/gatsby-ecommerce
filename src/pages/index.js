ec57857ceca35c7ca19efe69f832269bf9e795df
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-08-10 22:16:02 -0500

    Configs v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React from "react"
+import { Link } from "gatsby"
 
-export default function index() {
-  return (
-    <div>
-      <h1>Mi primer sitio con Gatsby</h1>
-    </div>
-  );
-}
+import { Layout, SEO } from "../components"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
