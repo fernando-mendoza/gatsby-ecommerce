cc61dfa4c5305f29aa3bad80d6316d280a718332
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-08-17 02:26:52 -0500

    Install Stripe Plugin and config(.env), products components created, and products componen added to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Jumbo } from "../components"
-import { SEO } from "../components"
-import styled from "styled-components"
+import { Jumbo, SEO, Products } from "../components"
 
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -15,26 +13,36 @@ export const query = graphql`
         }
       }
     }
+    allStripePrice {
+      edges {
+        node {
+          id
+          unit_amount
+          product {
+            name
+            metadata {
+              descripcion
+              wear
+              img
+            }
+          }
+        }
+      }
+    }
   }
 `
 
-const Button = styled.button`
-  widht: 8rem;
-  background-color: #98ca3f;
-  border: none;
-  border-radius: 10px;
-  color: ${props => props.color};
-  &:hover {
-    transform: scale(1.4);
-  }
-`
-
-const IndexPage = ({ data }) => (
-  <>
-    <SEO title="Home" />
-    <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <Button color="gray"> Comprar </Button>
-  </>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <>
+      <SEO title="Home" />
+      <Jumbo
+        description={data.allSite.edges[0].node.siteMetadata.description}
+      />
+      <Products products={data.allStripePrice.edges} />
+    </>
+  )
+}
 
 export default IndexPage
