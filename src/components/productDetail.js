f3ac92229fb05409e61f28cda35cf48a5de961ac
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-08-17 23:14:19 -0500

    Products component created(auto generate product page for each product using gatsby node js API)

---
+++ src/components/productDetail.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react"
+import priceFormat from "../utils/priceFormat"
+import {
+  Tag,
+  SizeButton,
+  QtyButton,
+  SizeSelect,
+  Button,
+  StyledProductDetail,
+  QtySelect,
+} from "../styles/components"
+import { SEO } from "./"
+
+export default function productDetail({
+  unit_amount: price,
+  id,
+  name,
+  product,
+  metadata,
+}) {
+  const formatedPrice = priceFormat(price)
+  const [size, setSize] = useState(2)
+  const [qty, setQty] = useState(1)
+  return (
+    <StyledProductDetail>
+      <SEO title={product.name} />
+      <img src={product.metadata.img} alt={product.name} />
+      <div>
+        <Tag>Popular</Tag>
+        <h2>{product.name}</h2>
+        <b>USD {formatedPrice}</b>
+        {product.metadata.wear && (
+          <SizeSelect selected={size}>
+            <SizeButton onClick={() => setSize(1)}>XS</SizeButton>
+            <SizeButton onClick={() => setSize(2)}>S</SizeButton>
+            <SizeButton onClick={() => setSize(3)}>M</SizeButton>
+            <SizeButton onClick={() => setSize(4)}>L</SizeButton>
+          </SizeSelect>
+        )}
+        <p>Cantidad:</p>
+        <QtySelect>
+          <QtyButton onClick={() => (qty > 1 ? setQty(qty - 1) : null)}>
+            -
+          </QtyButton>
+          <input type="text" disabled value={qty} />
+          <QtyButton onClick={() => setQty(qty + 1)}>+</QtyButton>
+        </QtySelect>
+      </div>
+    </StyledProductDetail>
+  )
+}
---
+++ src/templates/product.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { ProductDetail } from "../components"
+
+export default function product({ pageContext }) {
+  return <ProductDetail {...pageContext} />
+}
